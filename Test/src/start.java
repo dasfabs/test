8e8f1c1655e302b60655f5bfda2cf58f779bb96e
mhausding <martin@posteo.de>
2018-11-04 14:21:51 +0100

    neu

--- Test/src/start.java
+++ Test/src/start.java
@@ -3,14 +3,14 @@ public class start {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//alles it scheiﬂe
+		//alles it scheisse
 		System.out.println("Hello World");
 		System.out.println("Hello World ds vfdv ");
 
 		// Das ist ein Hammerkommentar von keinem Geringeren als Boris
 	}
 	
-	public void copyPaste() {
+	public void copyHaste() {
 		System.out.println("Hello World");
 		// Das ist von Martin.
 	}
